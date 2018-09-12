eb1c2928c3f0431adbae0329856201d696d8d214
Stewart ilondanga <stilondanga@gmail.com>
2018-09-12 10:04:41 +0300

    add js/scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -33,46 +33,46 @@ function solve() {
   }
   let month = first_num - 2;
 
-  /*switch(month) {
-      case 1:
-        month = "January";
-        break;
-      case 2:
-        month = "February";
-        break;
-      case 3:
-        month = "March";
-        break;
-      case 4:
-        month = "April";
-        break;
-      case 5:
-        month = "May";
-        break;
-      case 6:
-        month = "June";
-        break;
-      case 7:
-        month = "July";
-        break;
-      case 8:
-        month = "August";
-        break;
-      case 9:
-        month = "September";
-        break;
-      case 10:
-        month = "October";
-        break;
-      case 11:
-        month = "November";
-        break;
-      case 12:
-        month = "December";
-        break;
-    }
+  switch (month) {
+    case 1:
+      month = "January";
+      break;
+    case 2:
+      month = "February";
+      break;
+    case 3:
+      month = "March";
+      break;
+    case 4:
+      month = "April";
+      break;
+    case 5:
+      month = "May";
+      break;
+    case 6:
+      month = "June";
+      break;
+    case 7:
+      month = "July";
+      break;
+    case 8:
+      month = "August";
+      break;
+    case 9:
+      month = "September";
+      break;
+    case 10:
+      month = "October";
+      break;
+    case 11:
+      month = "November";
+      break;
+    case 12:
+      month = "December";
+      break;
+  }
 
-    const ans = document.getElementById('ans');
+  /*const ans = document.getElementById('ans');
     ans.innerHTML = `<b>Your Birthday is on ${second_num - 50} ${month}.</b>`;
   }
 
