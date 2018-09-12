e46c8d372ab79b6e0ac8edbdd27ff48778854e52
Stewart ilondanga <stilondanga@gmail.com>
2018-09-12 10:02:43 +0300

    add js/scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -24,70 +24,70 @@ function solve() {
   const user_array = user_input.split("");
   let first_num, second_num;
 
-  /*if (user_array.length < 4) {
+  if (user_array.length < 4) {
     first_num = user_array[0];
     second_num = user_array[1] + user_array[2];
-  } else {
-    first_num = user_array[0] + user_array[1];
-    second_num = user_array[2] + user_array[3];
-  }
-  let month = first_num - 2;
+    /*  } else {
+        first_num = user_array[0] + user_array[1];
+        second_num = user_array[2] + user_array[3];
+      }
+      let month = first_num - 2;
 
-  switch(month) {
-    case 1:
-      month = "January";
-      break;
-    case 2:
-      month = "February";
-      break;
-    case 3:
-      month = "March";
-      break;
-    case 4:
-      month = "April";
-      break;
-    case 5:
-      month = "May";
-      break;
-    case 6:
-      month = "June";
-      break;
-    case 7:
-      month = "July";
-      break;
-    case 8:
-      month = "August";
-      break;
-    case 9:
-      month = "September";
-      break;
-    case 10:
-      month = "October";
-      break;
-    case 11:
-      month = "November";
-      break;
-    case 12:
-      month = "December";
-      break;
-  }
+      switch(month) {
+        case 1:
+          month = "January";
+          break;
+        case 2:
+          month = "February";
+          break;
+        case 3:
+          month = "March";
+          break;
+        case 4:
+          month = "April";
+          break;
+        case 5:
+          month = "May";
+          break;
+        case 6:
+          month = "June";
+          break;
+        case 7:
+          month = "July";
+          break;
+        case 8:
+          month = "August";
+          break;
+        case 9:
+          month = "September";
+          break;
+        case 10:
+          month = "October";
+          break;
+        case 11:
+          month = "November";
+          break;
+        case 12:
+          month = "December";
+          break;
+      }
 
-  const ans = document.getElementById('ans');
-  ans.innerHTML = `<b>Your Birthday is on ${second_num - 50} ${month}.</b>`;
-}
+      const ans = document.getElementById('ans');
+      ans.innerHTML = `<b>Your Birthday is on ${second_num - 50} ${month}.</b>`;
+    }
 
-// Calculator
-function c(val) {
-document.getElementById("d").value=val;
-}
-function v(val) {
-  document.getElementById('d').value += val;
-}
-function e() {
-  try {
-    c(eval(document.getElementById('d').value))
-  }
-  catch(e) {
-    c('Error')
-  }
-}
+    // Calculator
+    function c(val) {
+    document.getElementById("d").value=val;
+    }
+    function v(val) {
+      document.getElementById('d').value += val;
+    }
+    function e() {
+      try {
+        c(eval(document.getElementById('d').value))
+      }
+      catch(e) {
+        c('Error')
+      }
+    }
